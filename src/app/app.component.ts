e15b08894812644cf6fed1b26ea16f38e0906447
Porther2057 <porther.lopez@viapin.local>
2025-01-20 10:39:33 -0500

    chore:Se traslado el contenido a dashboard-content.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardContentComponent } from "./dashboard-content/dashboard-content.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DashboardContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
