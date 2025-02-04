90f44ad8d377509efa530687e7382eba7f14a36e
Porther2057 <porther.lopez@viapin.local>
2025-02-04 16:08:05 -0500

    refactor: Se añadio metodo http

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication} from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    ...(appConfig.providers || [])
+  ]
+}).catch(err => console.error(err));
